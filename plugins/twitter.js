a8644920d320fe6556a048d6cb555a7b5ca38ba0
techwolf12 <christiaan@droiddev.nl>
2015-01-14 12:28:17 +0100

    Algoritm for tweets

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -7,11 +7,31 @@ var t = new Twitter({
   access_token_secret: process.env.TWIT_TOKEN_SECRET
 });
 var watchSymbols = ['@mediacollegelab', '#malab'];
+var tweets = [];
+var watchList = {
+	total : 0,
+	symbols : {}
+};
 
 var stream = t.stream('statuses/filter', { track: watchSymbols})
 
 stream.on('tweet', function (tweet) {
-	console.log(tweet)
+		var claimed = false;
+		if (tweet.text !== undefined) {
+			var text = tweet.text.toLowerCase();
+			_.each(watchSymbols, function (v) {
+				if (text.indexOf(v.toLowerCase()) !== -1) {
+					watchList.symbols[v]++;
+					claimed = true;
+				}
+			});
+			if (claimed) {
+				watchList.total++;
+				var i = watchList.total;
+				var tweetformat = "@" + tweet.user.screen_name + ": " + text;
+				console.log("New tweet: " + tweetformat);
+				console.log(watchList);
+			}
 })
 
 function postTweet(tweet) {
